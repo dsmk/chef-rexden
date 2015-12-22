837e93082e1bdf36cb351dba432fc7de4fe1aae0
David King <david@rexden.us>
2015-12-22 11:18:08 -0500

    Comment changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,8 @@
 # Cookbook Name:: rexden
 # Recipe:: default
 #
+# This is our default client recipe which is designed to be used by many different systems
+#
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 #
 if node['os'] == 'linux' 
