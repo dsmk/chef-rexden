d2609f1268ae21a2d3461ff9ba9907fed69750f2
David King <dsmk@hagrid.rexden.us>
2016-01-03 07:04:54 -0500

    Adding graphical login to dev list

---
+++ recipes/_graphical.rb
@@ -0,0 +1,28 @@
+bash 'yum groupinstall GNOME Desktop' do
+  user 'root'
+  group 'root'
+  code <<-EOC
+    yum groupinstall "GNOME Desktop" -y
+  EOC
+  not_if "yum grouplist installed | grep 'GNOME Desktop' "
+end
+
+bash 'enable graphical target' do
+  user 'root'
+  group 'root'
+  code <<-EOC
+    /usr/bin/systemctl set-default graphical.target
+  EOC
+  not_if "[ `/usr/bin/systemctl get-default` = graphical.target ]"
+end
+
+bash 'switch to graphical target' do
+  user 'root'
+  group 'root'
+  code <<-EOC
+    /usr/bin/systemctl isolate graphical.target
+  EOC
+  not_if "/usr/bin/systemctl list-units --type target | grep graphical.target"
+end
+
+# vi:ts=2 expandtab 
--- recipes/dev.rb
+++ recipes/dev.rb
@@ -1,4 +1,5 @@
 include_recipe 'rexden'
+include_recipe 'rexden::_graphical'
 include_recipe 'rexden::_dev_vagrant'
 include_recipe 'rexden::docker'
 
