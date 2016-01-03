37b57c4611dff3edf3a36961bae2245b3889e406
David King <dsmk@aragog.rexden.us>
2016-01-03 09:02:14 -0500

    Moving docker.rb to _docker.rb

--- recipes/dev.rb
+++ recipes/dev.rb
@@ -1,6 +1,6 @@
 include_recipe 'rexden'
 include_recipe 'rexden::_graphical'
 include_recipe 'rexden::_dev_vagrant'
-include_recipe 'rexden::docker'
+include_recipe 'rexden::_docker'
 
 # vi: expandtab ts=2 
