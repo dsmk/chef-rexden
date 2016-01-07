33572d0f15cd76b183ba692c68767b011f6bf6d5
David King <david@rexden.us>
2016-01-07 12:28:23 -0500

    Adding windows dev box

--- recipes/dev.rb
+++ recipes/dev.rb
@@ -1,6 +1,12 @@
 include_recipe 'rexden'
-include_recipe 'rexden::_graphical'
-include_recipe 'rexden::_dev_vagrant'
-include_recipe 'rexden::_docker'
+
+case node[:os]
+when 'linux'
+  include_recipe 'rexden::_graphical'
+  include_recipe 'rexden::_dev_vagrant'
+  include_recipe 'rexden::_docker'
+when 'windows'
+  include_recipe 'rexden::_dev_windows'
+end
 
 # vi: expandtab ts=2 
--- recipes/windows.rb
+++ recipes/windows.rb
@@ -1,5 +1,7 @@
 include_recipe 'chocolatey'
 
+#log "packages=#{node['chocolatey']['packages']}"
+
 # install the chocolatey packages
 node['chocolatey']['packages'].each do |p|
   chocolatey p
